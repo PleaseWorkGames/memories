31904e7cf5d154ba13eedbe6577aa6bf15457543
Dan Mooney <dan.r.mooney@gmail.com>
2018-01-07 11:01:58 -0600

    Update zoom sensitivity and have camera follow player

--- Assets/scripts/CameraController.cs
+++ Assets/scripts/CameraController.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    private Vector3 offset;
+
     public float cameraOffsetY;
     
     public GameObject targetDungeon;
+    
+    public Rigidbody2D player;
 
     public Camera camera;
     
@@ -20,15 +24,20 @@ public class CameraController : MonoBehaviour
     void Start()
     {
         updateTarget(targetDungeon);
+        offset = transform.position - player.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
         var orthographicSize = camera.orthographicSize;
-        orthographicSize += Input.GetAxis("Mouse ScrollWheel") * sensitivity * -1;
-        orthographicSize = Mathf.Clamp(orthographicSize, minOrthographicSize, maxOrthographicSize);
-        camera.orthographicSize = orthographicSize;
+        var mouseScroll = Input.GetAxis("Mouse ScrollWheel");
+
+        if (mouseScroll > 0 || mouseScroll < 0) {
+            orthographicSize += Input.GetAxis("Mouse ScrollWheel") * sensitivity * -1;
+            orthographicSize = Mathf.Clamp(orthographicSize, minOrthographicSize, maxOrthographicSize);
+            camera.orthographicSize = orthographicSize;
+        }
     }
 
     public void updateTarget(GameObject newTarget)
@@ -38,5 +47,11 @@ public class CameraController : MonoBehaviour
             new Vector3(targetDungeon.transform.position.x, targetDungeon.transform.position.y + cameraOffsetY,
             this.transform.position.z), this.transform.rotation
         );
+    }
+    
+    public void LateUpdate () 
+    {
+        // Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
+        camera.transform.position = player.transform.position + offset;
     }
 }
