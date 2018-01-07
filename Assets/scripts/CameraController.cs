24ec8a813f91061f370c12fa03cd12721edf346e
Dan Mooney <dan.r.mooney@gmail.com>
2018-01-07 10:11:07 -0600

    Add camera zoom with scroll wheel

--- Assets/scripts/CameraController.cs
+++ Assets/scripts/CameraController.cs
@@ -2,24 +2,41 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraController : MonoBehaviour {
-
+public class CameraController : MonoBehaviour
+{
     public float cameraOffsetY;
+    
     public GameObject targetDungeon;
 
+    public Camera camera;
+    
+    public int sensitivity;
+
+    public float minOrthographicSize;
+    
+    public float maxOrthographicSize;
+
     // Use this for initialization
-    void Start() {
+    void Start()
+    {
         updateTarget(targetDungeon);
     }
 
     // Update is called once per frame
-    void Update() {
-
+    void Update()
+    {
+        var orthographicSize = camera.orthographicSize;
+        orthographicSize += Input.GetAxis("Mouse ScrollWheel") * sensitivity;
+        orthographicSize = Mathf.Clamp(orthographicSize, minOrthographicSize, maxOrthographicSize);
+        camera.orthographicSize = orthographicSize;
     }
 
     public void updateTarget(GameObject newTarget)
     {
         this.targetDungeon = newTarget;
-        this.transform.SetPositionAndRotation(new Vector3(targetDungeon.transform.position.x,targetDungeon.transform.position.y + cameraOffsetY,this.transform.position.z),this.transform.rotation);
+        this.transform.SetPositionAndRotation(
+            new Vector3(targetDungeon.transform.position.x, targetDungeon.transform.position.y + cameraOffsetY,
+            this.transform.position.z), this.transform.rotation
+        );
     }
 }
