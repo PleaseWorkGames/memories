a2c1480dcfb39beca81be6017f5e2af59b7c973c
Dan Mooney <dan.r.mooney@gmail.com>
2018-01-17 19:22:09 -0600

    Remove and set dialogue on hitting exit sprite

--- Assets/scripts/ExitController.cs
+++ Assets/scripts/ExitController.cs
@@ -7,8 +7,11 @@ public class ExitController : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player")) {
-            // TODO - game over!
-            Debug.Log("Found the exit!");
+            collision.gameObject.GetComponent<PlayerController>().setDialogue("Oh yeah!  Now I remember");
+            this.GetComponent<SpriteRenderer>().enabled = false;
+            this.GetComponent<BoxCollider2D>().enabled = false;
+            
+            // TODO - game over - fade camera to black
         }
     }
 }
